3be24be721f5e286fe6903683c7543071f21ef24
Peter Bamsey <peter.bamsey@googlemail.com>
2022-01-15 18:05:07 +0000

    Add image building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import argparse
+import docker
 from pathlib import Path
 
 QUERY_URL = "https://index.docker.io"
@@ -41,22 +42,22 @@ def get_image_tags(query_url: str, auth_token: str, image_name: str) -> list:
     return request.json().get('tags', 'None')
 
 
-def load_image_build_configs(config_root: str) -> dict:
+def load_image_build_configs(config_root: str) -> list:
     """
     Walk a directory structure and build a dictionary that contains all the information needed to trigger the build of
     all the Dockerfiles that are found
     :param config_root: The root directory to look for image configs
     """
-    image_build_config: dict = {}
-    counter: int = 0
+    image_build_config: list = []
     for path in Path(config_root).rglob('Dockerfile'):
         # Check for a version file, two levels up, otherwise assume latest
         version_file_path = f'{path.resolve().parents[1]}/version'
+        docker_file_path = str(path.resolve())
+        version = 'latest'
         if Path(version_file_path).exists():
             with open(version_file_path, 'r') as file:
-                image_build_config[str(path.resolve())] = file.read().rstrip()
-        else:
-            image_build_config[str(path.resolve())] = 'latest'
+                version = file.read().rstrip()
+        image_build_config.append(dict({'Dockerfile': docker_file_path, 'version': version}))
 
     return image_build_config
 
@@ -69,4 +70,22 @@ if __name__ == '__main__':
     auth_token = get_auth_token(image_name=args.image, auth_url=AUTH_URL)
     tags = get_image_tags(query_url=QUERY_URL, auth_token=auth_token, image_name=args.image)
     print(tags)
-    print(load_image_build_configs('dockerfiles'))
+    image_build_configs = load_image_build_configs('dockerfiles')
+
+    print(image_build_configs)
+    docker_client = docker.from_env()
+
+
+    for config in image_build_configs:
+        print(config)
+        dockerfile_directory = str(Path(config.get('Dockerfile')).parent)
+        build_args = {'VERSION': config.get('version')}
+        build_result = docker_client.images.build(path=dockerfile_directory,
+                                                  buildargs=build_args,
+                                                  tag=config.get('version'))
+
+        for log in build_result[1]:
+            print(log, flush=True)
+
+    docker_client.close()
+
